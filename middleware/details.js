db8f657949b536ceb61b75334ecfc311d3d2f52c
Austin <austinandogola@gmail.com>
2022-07-03 19:08:15 +0300

    initial_set_up,save_tweet path

---
+++ middleware/details.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+const fetch = require('node-fetch');
+
+
+
+
+const tweet=async (id)=>{
+  const url2=`https://api.twitter.com/2/tweets?ids=${id}&tweet.fields=attachments,author_id,created_at&expansions=attachments.media_keys&media.fields=url`
+  const url=`https://api.twitter.com/2/tweets/${id}?tweet.fields=created_at,attachments,author_id,public_metrics,source&media.fields=url`
+  // const url=`https://api.twitter.com/2/tweets/${id}?tweet.fields=created_at,attachments,author_id,public_metrics`
+  try {
+    const response=await fetch(url,{
+      method:'GET',
+      headers:{
+        "Authorization": `Bearer ${process.env.twitter_Token}`
+      }
+    })
+    const resp=await response.json()
+
+    const tweet={}
+
+    tweet.data=resp['data']
+    if (resp['includes']) {
+      tweet.media=resp['includes']['media']
+    }
+
+
+    return tweet;
+  } catch (e) {
+    console.log(e.message);
+  }
+
+}
+
+const user=async (id)=>{
+  const url= `https://api.twitter.com/2/users/${id}?user.fields=name,username,location,profile_image_url`
+  try {
+    const response=await fetch(url,{
+      method:'GET',
+      headers:{
+        "Authorization": `Bearer ${process.env.twitter_Token}`
+      }
+    })
+
+    const resp=await response.json()
+    return resp.data
+  } catch (e) {
+    console.log(e.message);
+  }
+
+}
+
+
+
+
+module.exports = {tweet,user};
