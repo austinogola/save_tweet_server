21365512eb9ff8ca4bcb153266afe1ed64fb1be1
Austin <austinandogola@gmail.com>
2022-09-22 09:59:58 +0300

    clear cache

--- routes/screenshot.js
+++ routes/screenshot.js
@@ -11,7 +11,7 @@ router.post('/shot',async(req,res)=>{
   const browser=await puppeteer.launch({
     headless:true,
     defaultViewport: {width: 800, height: 900},
-    args: ['--no-sandbox']
+    args: ['--no-sandbox','--disable-setuid-sandbox']
   })
   const page=await browser.newPage()
   await page.goto(`https://tweet-shot-api.herokuapp.com/render/${name}`)
